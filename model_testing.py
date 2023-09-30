b38d7d5f8ef8145b59d55e88d7e64d929d4a27a3
Abdul Qadir <aq98123@gmail.com>
2023-09-30 15:03:10 +0500

    adding other 4 files

---
+++ model_testing.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+import numpy as np
+
+def preprocess_image(image):
+    image = tf.image.decode_image(image.read(), channels=3)
+    image = tf.image.convert_image_dtype(image, tf.float32)
+    image = tf.image.resize(image, (224, 224))
+    image = image.numpy()
+    return image
+
+
+model = tf.keras.models.load_model('lungcaner_detection2.h5')
+
+inp_image = preprocess_image("NORMAL2-IM-0281-0001.jpeg")
+
+model.predict(inp_image)
